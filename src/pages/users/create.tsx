af78bdbc2bdfde8579236cd5e48f3b6762f20b55
Marcos Vinicius <Marcos Vinicius>
2022-08-10 23:51:45 -0400

    feat: finalizando a page create users

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,4 +1,5 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, VStack } from "@chakra-ui/react";
+import { Input } from "../../components/Form/Input";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
 
@@ -17,7 +18,28 @@ export default function CreateUser() {
                bg="gray.800"
                p="8"
             >
-               
+               <Heading size="lg" fontWeight="normal">Criar usuário</Heading>
+
+               <Divider my="6" borderColor="gray.700" />
+
+               <VStack spacing="8">
+                  <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+                     <Input name="name" label="Nome Completo" />
+                     <Input name="email" type="email" label="E-mail" />
+                  </SimpleGrid>
+                  <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+                     <Input name="password" type="password"label="Senha" />
+                     <Input name="password_confirmation" type="password" label="Confirmação da senha" />
+                  </SimpleGrid>
+               </VStack>
+
+               <Flex mt="8" justify="flex-end">
+                  <HStack>
+                     <Button colorScheme="whiteAlpha">Cancelar</Button>
+                     <Button colorScheme="pink">Salvar</Button>
+                  </HStack>
+               </Flex>
+
             </Box>
          </Flex>
       </Box>
