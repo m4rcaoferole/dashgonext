a39e8d93dbe5a28605b0c2db95c904b6880c3db7
Marcos Vinicius <marcao.adsistema@gmail.com>
2022-09-24 20:50:45 -0400

    feat: usemutations react query

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import {
   Box,
   Button,
@@ -9,12 +8,19 @@ import {
   SimpleGrid,
   VStack,
 } from "@chakra-ui/react";
-import { Input } from "../../components/Form/Input";
-import { Header } from "../../components/Header";
-import { Sidebar } from "../../components/Sidebar";
 import { SubmitHandler, useForm } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
+import Link from "next/link";
+import { api } from "../../services/api";
+import { useRouter } from "next/router";
+
+import { useMutation } from "react-query";
+
+import { Input } from "../../components/Form/Input";
+import { Header } from "../../components/Header";
+import { Sidebar } from "../../components/Sidebar";
+import { queryClient } from "../../services/queryClient";
 
 type CreateUserFormData = {
   name: string;
@@ -24,13 +30,8 @@ type CreateUserFormData = {
 };
 
 const createUserFormSchema = yup.object().shape({
-  name: yup
-    .string()
-    .required("Nome Obrigatório"),
-  email: yup
-    .string()
-    .email("E-mail inválido")
-    .required("E-mail Obrigatório"),
+  name: yup.string().required("Nome Obrigatório"),
+  email: yup.string().email("E-mail inválido").required("E-mail Obrigatório"),
   password: yup
     .string()
     .required("Senha Obrigatória")
@@ -41,6 +42,23 @@ const createUserFormSchema = yup.object().shape({
 });
 
 export default function CreateUser() {
+  const router = useRouter()
+
+  const createUser = useMutation(async (user: CreateUserFormData) => {
+    const response = await api.post("users", {
+      user: {
+        ...user,
+        created_at: new Date(),
+      },
+    });
+
+    return response.data.user;
+  }, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('users')
+    }
+  });
+
   const {
     register,
     handleSubmit,
@@ -49,8 +67,12 @@ export default function CreateUser() {
     resolver: yupResolver(createUserFormSchema),
   });
 
-  const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
-    // console.log(values);
+  const handleCreateUser: SubmitHandler<CreateUserFormData> = async (
+    values
+  ) => {
+    await createUser.mutateAsync(values);
+
+    router.push('/users');
   };
 
   return (
@@ -78,6 +100,7 @@ export default function CreateUser() {
             <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
               <Input
                 name="name"
+                type="name"
                 label="Nome Completo"
                 errors={errors.name?.message}
                 {...register("name")}
@@ -115,7 +138,7 @@ export default function CreateUser() {
                   Cancelar
                 </Button>
               </Link>
-              <Button type="submit" colorScheme="pink" isLoading>
+              <Button type="submit" colorScheme="pink">
                 Salvar
               </Button>
             </HStack>
